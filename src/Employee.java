97a3b202d0313baaeaf59c937cdc73b8c7e837e7
1295957 <kissaroo@mail.ru>
2025-04-07 20:12:58 +0300

    coursework-1 first level

---
+++ src/Employee.java
@@ -0,0 +1,58 @@
+import java.util.Objects;
+
+public class Employee {
+    private String fullName;
+    private int department;
+    private int monthSalary;
+    private static int counter;
+    private int id;
+
+    public Employee(String fullName, int department, int monthSalary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.monthSalary = monthSalary;
+        this.id = ++counter;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getMonthSalary() {
+        return monthSalary;
+    }
+
+    public void setMonthSalary(int monthSalary) {
+        this.monthSalary = monthSalary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && monthSalary == employee.monthSalary && id == employee.id && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, monthSalary, id);
+    }
+
+    @Override
+    public String toString() {
+        return id + " ФИО:" + fullName + ", отдел:" + department +
+                ", заработная плата:" + monthSalary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,106 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee("Иванов Иван Иванович", 1, 127657);
+        employee[1] = new Employee("Борисов Борис Борисович", 2, 70096);
+        employee[2] = new Employee("Касмынина Анна Сергеевна", 3, 38457);
+        employee[3] = new Employee("Наумова Виктория Юрьевна", 4, 68651);
+        employee[4] = new Employee("Карпенко Кирилл Васильевич", 5, 30430);
+        employee[5] = new Employee("Иванина Алена Викторовна", 1, 43678);
+        employee[6] = new Employee("Петров Вячеслав Кириллович", 2, 97092);
+        employee[7] = new Employee("Трофимова Ирина Валерьевна", 3, 90650);
+        employee[8] = new Employee("Савушкин Петр Евстигнеевич", 4, 128670);
+
+        printEmployee(employee);
+        System.out.println("Общая сумма затрат на зп в месяц: " + calculateSalarySum(employee));
+        System.out.println("Сотрудник с минимальной заработной платой: " + getMinSalaryEmployee(employee));
+        System.out.println("Сотрудник с максимальной заработной платой: " + getMaxSalaryEmployee(employee));
+        System.out.println("Средняя зп в месяц: " + getTheAverageMonthSum(employee));
+        getEmployeeFullName(employee);
     }
+
+
+    public static void printEmployee(Employee[] employee) {
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                System.out.println(employee[i]);
+            }
+        }
+    }
+
+    public static int calculateSalarySum(Employee[] employee) {
+        int sum = 0;
+        for (Employee employees : employee) {
+            if (employees != null) {
+                sum += employees.getMonthSalary();
+            }
+        }
+        return sum;
+    }
+
+    public static String getMinSalaryEmployee(Employee[] employee) {
+        int minSalary = 100_000;
+        String employeeName = null;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] == null) {
+                break;
+            }
+            if (employee[i].getMonthSalary() != 0 && minSalary > employee[i].getMonthSalary()) {
+                minSalary = employee[i].getMonthSalary();
+                employeeName = employee[i].getFullName();
+            }
+        }
+        return employeeName;
+
+    }
+
+    public static String getMaxSalaryEmployee(Employee[] employee) {
+        int maxSalary = 0;
+        String employeeName = null;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] == null) {
+                break;
+            }
+            if (employee[i].getMonthSalary() != 0 && maxSalary < employee[i].getMonthSalary()) {
+                maxSalary = employee[i].getMonthSalary();
+                employeeName = employee[i].getFullName();
+            }
+        }
+        return employeeName;
+    }
+
+    public static double getTheAverageMonthSum(Employee[] employee) {
+        int monthSalarySum = calculateSalarySum(employee);
+        int numberOfPeople;
+        double averageMonthSum = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                numberOfPeople = employee[i].getId();
+                averageMonthSum = (double) monthSalarySum / numberOfPeople;
+            }
+        }
+        return averageMonthSum;
+    }
+
+    public static void getEmployeeFullName(Employee[] employee) {
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null)
+                System.out.println(employee[i].getFullName());
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
